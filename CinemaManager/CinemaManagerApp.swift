6550a6886d55eab6b798b9d5580cb67fc29351d8
Stefan de Gier <stefan_de_gier@outlook.com>
2024-12-15 13:23:52 +0100

    Initial Commit

---
+++ CinemaManager/CinemaManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaManagerApp.swift
+//  CinemaManager
+//
+//  Created by Stefan de Gier on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
