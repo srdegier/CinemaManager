643a997f2d3aedd03e44961fce55937098193b27
Stefan de Gier <stefan_de_gier@outlook.com>
2024-12-18 15:56:30 +0100

    Added SwiftData models

--- CinemaManager/CinemaManagerApp.swift
+++ CinemaManager/CinemaManagerApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct CinemaManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            // movie
+            Movie.self,
+            MovieRoom.self,
+            ReservationSeat.self,
+            ReservationRowSeats.self,
+            // room
+            Room.self,
+            RoomConfiguration.self,
+            RowSeats.self,
+            Seat.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
